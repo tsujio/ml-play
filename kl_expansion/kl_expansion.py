ccfe285065ff8b1196e9cb8edcfe48f0d1a71b85
tsujio <ntsujio@gmail.com>
2018-02-27 00:49:29 +0900

    Update kl expansion

--- kl_expansion/kl_expansion.py
+++ kl_expansion/kl_expansion.py
@@ -17,12 +17,15 @@ if __name__ == '__main__':
     iris = datasets.load_iris()
 
     A = kl_expansion(2, iris.data)
+
+    X = np.array([np.dot(A, x) for x in iris.data])
+
+    # compare with sklearn's pca result
     from sklearn import decomposition
     pca = decomposition.PCA(n_components = 2)
     pca.fit(iris.data)
-#    A = pca.components_
-
-    X = np.array([np.dot(A, x) for x in iris.data])
+    print(pca.components_)
+    print(np.dot(np.array([[1, 0], [0, -1]]), A)) # flip A upside down
 
     plt.scatter(X[:, 0], X[:, 1], c=iris.target)
     plt.show()
