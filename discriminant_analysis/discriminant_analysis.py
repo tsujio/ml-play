721af2cf5b6d7cf581094f70d7d4d806b10f752f
tsujio <ntsujio@gmail.com>
2018-03-04 00:05:13 +0900

    Implement discriminant analysis

--- discriminant_analysis/discriminant_analysis.py
+++ discriminant_analysis/discriminant_analysis.py
@@ -2,46 +2,9 @@ from matplotlib import pyplot as plt
 import numpy as np
 
 
-def _discriminant_analysis(X, y):
-    # between-class covariance matrix
-    cov_b = np.zeros((X.shape[1], X.shape[1]))
-    # within-class covariance matrix
-    cov_w = np.zeros((X.shape[1], X.shape[1]))
-
-    m = np.array([np.sum(X[:, i]) / X.shape[0]
-                  for i in range(X.shape[1])])
-    M = np.zeros(X.shape[1])
-    for yi in np.unique(y):
-        Xi = X[y == yi]
-        pi = len(y[y == yi]) / len(y)
-        mi = np.array([np.sum(Xi[:, i]) / Xi.shape[0]
-                       for i in range(Xi.shape[1])])
-        covi = np.sum([np.dot((x - mi)[:, np.newaxis],
-                              (x - mi)[np.newaxis, :]) for x in Xi],
-                      axis=0) / Xi.shape[0]
-#        covi = np.sum([
-#            (x - mi).reshape(Xi.shape[1], 1) * (x - mi).reshape(1, Xi.shape[1])
-#            for x in Xi
-#        ], axis=0) / Xi.shape[0]
-#        print(covi)
-        cov_w += pi * covi
-
-        cov_b += pi * np.dot((mi - m)[:, np.newaxis], (mi - m)[np.newaxis, :])
-#        cov_b += pi * (mi - m).reshape(Xi.shape[1], 1) * (mi - m).reshape(1, Xi.shape[1])
-
-        M += mi / len(np.unique(y))
-
-    la, v = np.linalg.eig(
-        np.dot(np.linalg.inv(cov_w), cov_b)
-#        np.linalg.inv(cov_w) * cov_b
-    )
-
-    w = v.T[np.argmax(la)]
-    return lambda x: -w[0]/w[1] * x + -w[0]/w[1] * M[0] + M[1]#v[np.argmax(la)]
-
-
 def discriminant_analysis(X, y):
-    X0, X1 = X[y == 0], X[y == 1]
+    classes = np.unique(y)
+    X0, X1 = X[y == classes[0]], X[y == classes[1]]
     m0, m1 = X0.mean(axis=0), X1.mean(axis=0)
     Sw = np.sum(
         [np.sum([np.dot((x - mi)[:, np.newaxis],
@@ -61,52 +24,6 @@ def discriminant_analysis(X, y):
     w = v.T[np.argmax(la)]
     m = (m0 + m1) / 2
     return lambda x: -w[0]/w[1] * x + w[0]/w[1] * m[0] + m[1]
-
-    for yi in np.unique(y):
-        Xi = X[y == yi]
-        mi = Xi.mean(axis=0)
-
-        Sw += np.sum([np.dot((x - mi)[:, np.newaxis],
-                             (x - mi)[np.newaxis, :]) for x in Xi],
-                     axis=0)
-#    Sb = () * 
-
-    # between-class covariance matrix
-    cov_b = np.zeros((X.shape[1], X.shape[1]))
-    # within-class covariance matrix
-    cov_w = np.zeros((X.shape[1], X.shape[1]))
-
-    m = np.array([np.sum(X[:, i]) / X.shape[0]
-                  for i in range(X.shape[1])])
-    M = np.zeros(X.shape[1])
-    for yi in np.unique(y):
-        Xi = X[y == yi]
-        pi = len(y[y == yi]) / len(y)
-        mi = np.array([np.sum(Xi[:, i]) / Xi.shape[0]
-                       for i in range(Xi.shape[1])])
-        covi = np.sum([np.dot((x - mi)[:, np.newaxis],
-                              (x - mi)[np.newaxis, :]) for x in Xi],
-                      axis=0) / Xi.shape[0]
-#        covi = np.sum([
-#            (x - mi).reshape(Xi.shape[1], 1) * (x - mi).reshape(1, Xi.shape[1])
-#            for x in Xi
-#        ], axis=0) / Xi.shape[0]
-#        print(covi)
-        cov_w += pi * covi
-
-        cov_b += pi * np.dot((mi - m)[:, np.newaxis], (mi - m)[np.newaxis, :])
-#        cov_b += pi * (mi - m).reshape(Xi.shape[1], 1) * (mi - m).reshape(1, Xi.shape[1])
-
-        M += mi / len(np.unique(y))
-
-    la, v = np.linalg.eig(
-        np.dot(np.linalg.inv(cov_w), cov_b)
-#        np.linalg.inv(cov_w) * cov_b
-    )
-
-    w = v.T[np.argmax(la)]
-    return lambda x: -w[0]/w[1] * x + -w[0]/w[1] * M[0] + M[1]#v[np.argmax(la)]
-
 
 
 if __name__ == '__main__':
